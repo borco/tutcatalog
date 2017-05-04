b812eeca6264ba72a1deea30ad56cf8cae4532e0
Ioan Calin <iborco@gmail.com>
2017-05-04 16:29:28 +0300

    refs #2 - added a text edit to the log widget; connected log actions

--- src/libtc/tc/ui/logwidget.cpp
+++ src/libtc/tc/ui/logwidget.cpp
@@ -3,6 +3,8 @@
 #include "theme.h"
 
 #include <QAction>
+#include <QTextEdit>
+#include <QVBoxLayout>
 
 namespace tc {
 namespace ui {
@@ -10,7 +12,8 @@ namespace ui {
 class LogWidgetPrivate: public QObject
 {
     Q_DECLARE_PUBLIC(LogWidget)
-    LogWidget* const q_ptr;
+    LogWidget* const q_ptr { nullptr };
+    QTextEdit* m_edit { nullptr };
     QList<QAction*> m_dockToolBarActions;
     QList<QAction*> m_appToolBarActions;
 
@@ -19,15 +22,36 @@ class LogWidgetPrivate: public QObject
         q->setWindowTitle(tr("Messages"));
         q->setWindowIcon(Pixmap::fromFont(Theme::MaterialFont, "\uE8B0", Theme::MainToolBarIconSize, Theme::MainToolBarIconColor));
 
+        m_edit = new QTextEdit;
+        m_edit->setReadOnly(true);
+        m_edit->setFrameStyle(QFrame::NoFrame);
+
+        auto layout = new QVBoxLayout;
+        layout->setMargin(0);
+        layout->setSpacing(0);
+        layout->addWidget(m_edit);
+
+        q->setLayout(layout);
+
         auto action = new QAction;
         action->setToolTip(tr("Clear all messages"));
         action->setIcon(Pixmap::fromFont(Theme::MaterialFont, "\uE872", Theme::DockToolBarIconSize, Theme::DockToolBarIconColor));
+        connect(action, &QAction::triggered, m_edit, &QTextEdit::clear);
         m_dockToolBarActions.append(action);
 
+        bool defaultWrapText = true;
         action = new QAction;
         action->setToolTip(tr("Wrap long lines"));
+        action->setCheckable(true);
         action->setIcon(Pixmap::fromFont(Theme::DefaultFont, "\u2BA8", Theme::DockToolBarIconSize, Theme::DockToolBarIconColor));
+        action->setChecked(defaultWrapText);
+        wrapText(defaultWrapText);
+        connect(action, &QAction::toggled, this, &LogWidgetPrivate::wrapText);
         m_dockToolBarActions.append(action);
+    }
+
+    void wrapText(bool value) {
+        m_edit->setWordWrapMode(value ? QTextOption::WordWrap : QTextOption::NoWrap);
     }
 };
 
