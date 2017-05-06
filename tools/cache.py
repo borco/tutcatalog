dda4083e47c6a15f696bd5dbbb1a8d4ce2c42151
Ioan Calin <iborco@gmail.com>
2017-05-06 13:41:46 +0300

    refs #5 - added extra argument to set the title prefix

--- tools/cache.py
+++ tools/cache.py
@@ -5,14 +5,14 @@ import sqlite3
 import os
 
 def create_database(args):
-    print("generating {} tutorials in {}".format(args.tutorials, args.file))
+    print("generating {} tutorials in {}, using tutorial name prefix {}".format(args.count, args.file, args.title))
 
     conn = sqlite3.connect(args.file)
     c = conn.cursor()
     c.execute("DROP TABLE IF EXISTS tutorials")
     c.execute("CREATE TABLE tutorials (id INTEGER PRIMARY KEY, title, publisher, authors)")
-    for i in range(args.tutorials):
-        title = "Title {}".format(i + 1)
+    for i in range(args.count):
+        title = "{} {}".format(args.title, i + 1)
         publisher = "Publisher {}".format(i % 3 + 1)
         authors = "Author {}".format(i + 1) if i % 5 == 0 else "Author {}, Author {}".format(i % 5, i % 5 + 1)
         c.execute("INSERT INTO tutorials (title, publisher, authors) VALUES ('{}', '{}', '{}')".format(title, publisher, authors))
@@ -20,10 +20,12 @@ def create_database(args):
     conn.close()
 
 if __name__ == "__main__":
-    default_file = "c:/sources/dummy.sqlite" # tutcatalog cache
-    default_tutorials = 10000
+    default_file = "c:/sources/tutcatalog-dummy-cache.sqlite" # tutcatalog cache
+    default_count = 10000
+    default_title = "Title"
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--tutorials", help="number of tutorials to generate (default: {})".format(default_tutorials), type=int, default=default_tutorials, metavar="N")
+    parser.add_argument("-c", "--count", help="number of tutorials to generate (default: {})".format(default_count), type=int, default=default_count, metavar="N")
     parser.add_argument("-f", "--file", help="output file (default: {})".format(default_file), default=default_file)
+    parser.add_argument("-t", "--title", help="tutorial title prefix (default: {})".format(default_title), default=default_title)
     args = parser.parse_args()
     create_database(args)
